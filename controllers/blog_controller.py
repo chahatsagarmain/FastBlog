a065f67e200b520a9fffd64ba235c8f1c75d2e56
chahatsagarmain <chahatsagar2003@gmail.com>
2024-05-13 04:31:56 +0530

    blog crud

--- controllers/blog_controller.py
+++ controllers/blog_controller.py
@@ -2,8 +2,27 @@ from motor.motor_asyncio import AsyncIOMotorDatabase
 from models.models import User
 from datetime import datetime
 from bson import ObjectId
+from models.models import Blog
 
 async def get_all_blogs_controller(db : AsyncIOMotorDatabase):
     blogs_coll = db.get_collection("blogs")
     blogs = await blogs_coll.find({}).to_list(length=None)
     return blogs
+
+async def create_blog_controller(db : AsyncIOMotorDatabase ,blog : Blog):
+    users_coll = db.get_collection("users")
+    blogs_coll = db.get_collection("blogs")
+    
+    author = await users_coll.find_one({"id" : ObjectId(blog.author)})
+    
+    if not author:
+        return None 
+    
+    inserted_blog = await blogs_coll.insert_one(blog)
+    
+    await users_coll.update_one(
+        {"_id": ObjectId(blog.author)},
+        {"$addToSet": {"blogs": inserted_blog.inserted_id}}
+    )
+    
+    return inserted_blog.inserted_id
--- routes/blog_routes.py
+++ routes/blog_routes.py
@@ -3,7 +3,7 @@ from db.connect import connect_to_db
 from controllers import blog_controller
 from models.models import Blog
 from fastapi.responses import JSONResponse
-from fastapi.exceptions import HTTPException
+from bson import json_util
 
 blog_router = APIRouter()
 client, db = connect_to_db()
@@ -13,4 +13,16 @@ async def get_all_blogs():
     global db
     blogs = await blog_controller.get_all_blogs_controller(db)
     blogs_json = [Blog(**blog).model_dump_json() for blog in blogs]
-    return R    
+    return JSONResponse(content={"data" : blogs_json},status_code=200)
+
+@blog_router.post("/blog")
+async def create_blog(blog : Blog):
+    global db 
+    try:
+        inserted_blog = await blog_controller.create_blog_controller(db,blog)
+        if not inserted_blog:
+            return JSONResponse(content={"message" : "blog not inserted"},status_code=400)
+        return JSONResponse(content={"message" : "blog inserted" , "data" : json_util.dumps(inserted_blog)})
+    except Exception as e:
+        return JSONResponse(content={"detail": "Internal Server Error"}, status_code=500)
+    
